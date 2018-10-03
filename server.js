e2c186d22fbe4a67dc21a1c75b278c212ae2efbe
tom balcaen <tom.balcaen88@gmail.com>
2018-10-03 06:25:49 +0200

    dist was in gitignore file

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(allowCrossDomain);
 app.use(bodyParser.json());
 
 // Create link to Angular build directory
-var distDir = __dirname + "/dist/";
+var distDir = __dirname + "/front-end/dist/";
 app.use(express.static(distDir));
 
 mongodb.MongoClient.connect(process.env.MONGODB_URI || uri, { useNewUrlParser: true }, function (err, client) {
