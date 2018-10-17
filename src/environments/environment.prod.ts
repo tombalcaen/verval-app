38c462faf7cfd69e89e1827c29587fc4715971c8
tom balcaen <tom.balcaen88@gmail.com>
2018-10-17 22:51:43 +0200

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  connection_uri: 'https://ripe-avocado.herokuapp.com/'
+  connection_uri: 'https://ripe-avocado.herokuapp.com/',
+  publicKey: "BDQ_GMoYMQr4HKsUCksZJDs_KGXBUNzTyaGtqapM7bBpFWIrECcXlP-zzmEhPQ5nIfH2iwNlZ3piKdSTRumqsyM",
+  privateKey: "W4DuePTQNCKXXn-opavZ3BX5E8BPVGIOJ_rDAes9W8M"
 };
